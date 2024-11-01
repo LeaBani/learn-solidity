90ba5c979daf473a7709738c1970d4e4e3fe3b44
LeaBani <leabani92@gmail.com>
2024-11-01 19:45:38 +0100

    init

---
+++ exercice-solidity.sol
@@ -0,0 +1,13 @@
+// fist exercice with tutorial
+
+pragma solidity >=0.8.2 <0.9.0;
+
+// create contract that stores and return the data
+// step: 1. receive information, 2. store information (data integers), 3. return
+
+contract simpleStorage {
+    uint storeData;
+    string names;
+    bool switchON;
+
+}
